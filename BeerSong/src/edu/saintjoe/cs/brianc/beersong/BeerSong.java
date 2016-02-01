6184d8e34d8569278508f1c384ccdcc993b6b482
Brian Capouch <brianc@palaver.net>
2016-02-01 14:47:36 -0500

    Initial commit

---
+++ BeerSong/src/edu/saintjoe/cs/brianc/beersong/BeerSong.java
@@ -0,0 +1,17 @@
+package edu.saintjoe.cs.brianc.beersong;
+
+public class BeerSong {
+
+	public static void main(String[] args) {
+		int beernum = 99;
+		String word = "bottles";
+		
+		while (beernum > 0) {
+			if (beernum == 1) {
+				word = "bottle";
+				}
+		}
+
+	}
+
+}
