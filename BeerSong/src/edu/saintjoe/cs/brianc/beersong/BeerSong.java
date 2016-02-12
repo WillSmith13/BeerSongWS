fd8b4ca2a27f1622dcb64dadb026268e547c8079
wills <wills@localhost>
2016-02-12 14:23:27 -0500

    Bug Fixed

--- BeerSong/src/edu/saintjoe/cs/brianc/beersong/BeerSong.java
+++ BeerSong/src/edu/saintjoe/cs/brianc/beersong/BeerSong.java
@@ -6,16 +6,16 @@ public class BeerSong {
 		int beerNum = 99;
 		String word = "bottles";
 		while (beerNum > 0) { // While there's bottles 
-			if (beerNum == 1) { // Correct for one bottle 
-				word = "bottle";
-				} // endif
-			System.out.println(beerNum + "" + " of beer on the wall");
-			System.out.println(beerNum + "" + word + " of beer");
+			System.out.println(beerNum + " " + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer");
 			System.out.println("Take one down.");
 			System.out.println("Pass it around");
 			beerNum = beerNum - 1;
+			if (beerNum == 1) { // Correct for one bottle 
+				word = "bottle";
+				} // endif
 			if (beerNum > 0) { // Adjust for case of no bottles
-				System.out.println(beerNum + "" + word + "beer on the wall");
+				System.out.println(beerNum + " " + word + " of beer on the wall");
 			} else {
 				System.out.println("No more bottles of beer on the wall");
 			} // end else
