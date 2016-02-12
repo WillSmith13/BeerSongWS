9c0f7096be86cd0cd93b9df59207e3e3ca952d2f
wills <wills@localhost>
2016-02-12 14:04:43 -0500

    Updated Beer Code

--- BeerSong/src/edu/saintjoe/cs/brianc/beersong/BeerSong.java
+++ BeerSong/src/edu/saintjoe/cs/brianc/beersong/BeerSong.java
@@ -3,17 +3,25 @@ package edu.saintjoe.cs.brianc.beersong;
 public class BeerSong {
 
 	public static void main(String[] args) {
-		int beernum = 99;
+		int beerNum = 99;
 		String word = "bottles";
-		
-		while (beernum > 0) {
-			if (beernum == 1) {
+		while (beerNum > 0) {
+			if (beerNum == 1) {
 				word = "bottle";
-				}
-			System.out.println("Hi mom " + beernum);
-			beernum = beernum - 1;
-		}// end while
+				} // endif
+			System.out.println(beerNum + "" + " of beer on the wall");
+			System.out.println(beerNum + "" + word + " of beer");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around");
+			beerNum = beerNum - 1;
+			if (beerNum > 0) {
+				System.out.println(beerNum + "" + word + "beer on the wall");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			} // end else
+			
+		} // end while
 
-	}//end main
+	} //end main
 
-}//end class
+} //end class
